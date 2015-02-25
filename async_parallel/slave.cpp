c47336da0b331d8a60899a607b3d4ed9f8731169
Andrew Yang <aqy8je@virginia.edu>
2015-02-25 05:23:49 -0500

    Pseudo-code probably finished. Implemented beginning states for master, minus select call.

--- async_parallel/slave.cpp
+++ async_parallel/slave.cpp
@@ -11,106 +11,112 @@ void setup();
 void teardown();
 void emscriptem_main(int32_t& s, int32_t& k, int32_t& r_s, bool& sent, bool& c_recv, int32_t& c, bool& loop);
 
-int main() {
-   setup();
+int main(int argc, char *argv[])
+{
+    setup();
 
-	int32_t s=0;
-	int32_t r_s=0;
-	int32_t c;
-	int32_t k;
-	bool sent=false;
-	bool c_recv=false;
-	bool loop=true;
-	// Initialize constants
-   while(loop)
-   {
-      emscriptem_main(s,k,r_s,sent,c_recv,c,loop); // pass in variables for constants by reference
-   }
+    int32_t s=0;
+    int32_t r_s=0;
+    int32_t c;
+    int32_t k;
+    bool sent=false;
+    bool c_recv=false;
+    bool loop=true;
+    // Initialize constants
+    while(loop)
+    {
+        emscriptem_main(s,k,r_s,sent,c_recv,c,loop); // pass in variables for constants by reference
+    }
 
-   teardown();
+    teardown();
 
-   return 0;
+    return 0;
 }
 
-void setup() {
-   // Setup socket stuff.
-   // Initialize arrays.
+void setup()
+{
+    // Setup socket stuff.
+    // Initialize arrays.
 }
 
-void teardown() {
-   // Close connections.
-   // Delete stuff allocated with new.
+void teardown()
+{
+    // Close connections.
+    // Delete stuff allocated with new.
 }
 
-void build_fd_table() {
-   // Build the fd table. To be called after EVERY select.
+void build_fd_table()
+{
+    // Build the fd table. To be called after EVERY select.
 }
 
-void relax() {
-	// Relax the data
+void relax()
+{
+    // Relax the data
 }
 
-void emscriptem_main(int32_t& s, int32_t& k, int32_t& r_s, bool& sent, bool& c_recv, int32_t& c, bool& loop) { // add constants to signature
-	int n;
+void emscriptem_main(int32_t& s, int32_t& k, int32_t& r_s, bool& sent, bool& c_recv, int32_t& c, bool& loop)   // add constants to signature
+{
+    int n;
 
-	if(s==0) // Initialized
-	{
-		if(!sent)
-		{
-			// [SEND] id
-			sent=true;
-		}
-		if(r_s==0)
-		{
-			// n= read(); //information
-			if(n==4*sizeof(int32_t))// [TODO] this is where we'd combine things into a struct
-			{
-				// store constants
-				r_s=1;
-			}
-		}
-		if(r_s==1)
-		{
-			//n=read(); //data block
-			if(n==0) // Change 0 to send_size!
-			{
-				// store data block
-				s=1;
-				c_recv=true;
-				return;
-			}
-		}
-	}
+    if(s==0) // Initialized
+    {
+        if(!sent)
+        {
+            // [SEND] id
+            sent=true;
+        }
+        if(r_s==0)
+        {
+            // n= read(); //information
+            if(n==4*sizeof(int32_t))// [TODO] this is where we'd combine things into a struct
+            {
+                // store constants
+                r_s=1;
+            }
+        }
+        if(r_s==1)
+        {
+            //n=read(); //data block
+            if(n==0) // Change 0 to send_size!
+            {
+                // store data block
+                s=1;
+                c_recv=true;
+                return;
+            }
+        }
+    }
 
-	if(s==1)	// Command mode
-	{
-		if(c_recv) // Receiving command
-		{
-			// n=read(); // command
-			if(n==2*sizeof(int32_t)) // what I believe is the size of the command
-			{
-				// the stuff's read into an array, so grab:
-				// c
-				// k
-			}
-			c_recv=false;
-		}
-		else // Acting on command
-		{
-			if(c==0)
-			{
-				// send row
-				c_recv=true;
-			}
-			if(c==1)
-			{
-				// n=read(); // broadcast row
-				if(n==0) // vertex_total*sizeof(int32_t)
-				{
-					relax();
-					// send ack
-				}
-			}
-		}	
-	}
+    if(s==1)	// Command mode
+    {
+        if(c_recv) // Receiving command
+        {
+            // n=read(); // command
+            if(n==2*sizeof(int32_t)) // what I believe is the size of the command
+            {
+                // the stuff's read into an array, so grab:
+                // c
+                // k
+            }
+            c_recv=false;
+        }
+        else // Acting on command
+        {
+            if(c==0)
+            {
+                // send row
+                c_recv=true;
+            }
+            if(c==1)
+            {
+                // n=read(); // broadcast row
+                if(n==0) // vertex_total*sizeof(int32_t)
+                {
+                    relax();
+                    // send ack
+                }
+            }
+        }
+    }
 }
